c31c7d601385f9a989f7e7a8d9330c41afdfe0e1
eliaquim <eliaquim_10@hotmail.com>
2019-02-07 04:11:53 -0300

    1.1 Aumento da base

--- teste.py
+++ teste.py
@@ -19,7 +19,8 @@ model.evaluate(x_test, y_test)'''
 import csv
 
 def readBase(csvFile = str):
-    base = [0]*6
+    classes = [0]*6
+    base = []
     i= False
     j=0
     with open(csvFile, newline='\n',encoding='utf-8') as csvFile:
@@ -30,15 +31,13 @@ def readBase(csvFile = str):
             try:
                 if(i):
                     temp = int(row[0])
-                    if(temp==4):
-                        print(row[1])
-                    base[temp - 1]+=1
+                    classes[temp - 1]+=1
+                    base.append(classes.copy())
                 else:
-                    print(row)
                     i = True
             except IndexError:
                 pass
-        return base
+    return base
 
 
 def writes(path,base):
@@ -47,6 +46,45 @@ def writes(path,base):
         for w in base:
             csv_file.writelines(w[0]+';'+w[1]+'\n')
 
-path = 'data_set\data_base_dilva_1000.csv'
+def coluna(base, c =int):
+    column = []
+    for w in base:
+        column.append(w[c-1])
+    return column
+
+def mostra_grafico(base,path):
+    import matplotlib.pyplot as plt
+
+    pos = coluna(base,1)
+    neg = coluna(base,2)
+    amb = coluna(base,3)
+    neu = coluna(base,4)
+    des = coluna(base,5)
+    iro = coluna(base,6)
+
+    tempos = range(1, len(base) + 1)
+    plt.plot(tempos, pos, 'k', label='Positivo')
+    plt.plot(tempos, neg, 'b', label='Negativo')
+    plt.plot(tempos, amb, 'r', label='Ambos')
+    plt.plot(tempos, neu, 'p', label='Neutro')
+    plt.plot(tempos, des, 'g', label='Descarte')
+    plt.plot(tempos, iro, 'y', label='Ironia')
+    plt.title('classes')
+    plt.xlabel('opiniao')
+    plt.ylabel('classes')
+    plt.legend()
+
+    plt.show()
+    plt.clf()   # clear figure
+
+path = 'data_set\data_base_d_1000.csv'
 base = readBase(path)
-print(base)
+
+mostra_grafico(base,path)
+
+path = 'data_set\data_base_v_a_2000.csv'
+base = readBase(path)
+
+mostra_grafico(base,path)
+
+
