8cc1058394709c544bcdb15e663c7fff0d47af15
eliaquim <eliaquim_10@hotmail.com>
2019-05-03 19:14:09 -0300

    1.1 v Aumento da base

---
+++ executaveis_teste/crnn-master/cPickle.py
@@ -0,0 +1,5 @@
+import pickle
+class cPickle(pickle):
+    def __init__(self):
+        def a():
+            print('Ok')
---
+++ executaveis_teste/crnn-master/run.sh
@@ -0,0 +1,8 @@
+# preprocess
+python process_sst2_data.py /media/vdvinh/25A1FEDE380BDADA/data/GoogleNews-vectors-negative300.bin data/stanfordSentimentTreebank
+\pcc\word2vecs\GoogleNews-vectors-negative300.bin
+# run
+THEANO_FLAGS=mode=FAST_RUN,device=cuda0,floatX=float32 python sst2_cnn_rnn.py
+
+THEANO_FLAGS=mode=FAST_RUN,device=cuda,floatX=float32 python sst2_cnn_rnn_keras1.py
+
